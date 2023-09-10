34461d9972cdaccbfd85280604904f0bb73eb0f7
Fatma2645 <142684037+Fatma2645@users.noreply.github.com>
2023-09-10 10:48:14 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+main.py
+
+class person:
+    #Attributes
+    name="fatma"
+    age=15
+    def is_adult(self):
+       if self.age>=18 :
+         print("You have too much responsibilities")
+       else:
+         print("Lucky you")
+first_person.is_adult()
+first_person=person()
+print(first_person.name)
+print(first_person.age)
+
+#method
+def is_adult(self):
+    if self.age>=18 :
+         print("You have too much responsibilities")
+    else:
+          print("Lucky you")
+first_person.is_adult()
+
+
+def str(self):
+    return f"my name is {self.name} and i am {self.age} years old"
+    first_person=person2("fatma",15)
+    print(first_person.name)
+    print(first_person.age)
+    print(first_person.is_adult())
+    print(first_person)
+
+
+
+
+
+
+
+                  
+
+        
+
+    
+
+
+
